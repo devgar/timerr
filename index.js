8475cb1c6df7db7ff3dfdc31dfb61a0dc8aa3c4c
Edgar Albalate <dev.gardo@gmail.com>
2020-05-07 09:08:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+[_, _ ,time, err] = process.argv
+time = parseInt(time)
+time = time || 7
+err = parseInt(err)
+err = err || 1
+console.log(`Exiting in ${time}s with error #${err}`)
+
+setTimeout(() => process.exit(1), 7 * 1000)
