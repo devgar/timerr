52c828cde0871140c817c32eb5e939bc33492541
Edgar Albalate <dev.gardo@gmail.com>
2020-09-18 17:43:01 +0200

    timerr now accepts time and error code as arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::env;
+use std::{thread::sleep, time::Duration};
+
+fn parse(arg: String) -> Option<u32> {
+    Some(arg.parse::<u32>().expect("Argument type must be an unsigned integer"))
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("Got args: {:?}", args);
+    let time: u32 = env::args().nth(1).and_then(parse).unwrap_or(6);
+    let err : u32 = env::args().nth(2).and_then(parse).unwrap_or(1);
+    sleep(Duration::from_secs(time as u64));
+    std::process::exit(err as i32);
 }
