a4a68cd10d459598cfbacfcd940e1537035b0eab
Edgar Albalate Ibañez <dev.gardo@gmail.com>
2020-09-18 17:01:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-[_, _ ,time, err] = process.argv
-time = parseInt(time)
+
+const [_, _ ,time, err] = process.argv
+let time = parseInt(time)
 time = time || 7
-err = parseInt(err)
+let err = parseInt(err)
 err = err || 1
 console.log(`Exiting in ${time}s with error #${err}`)
 
-setTimeout(() => process.exit(1), 7 * 1000)
+setTimeout(() => process.exit(err), time * 1000)
