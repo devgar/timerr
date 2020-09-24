1ca2e0ffc5d5bd647128df81e1a2dc81c120a6df
Edgar Albalate <dev.gardo@gmail.com>
2020-09-24 19:16:08 +0200

    Fixed variable definitions error

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const [_, _ ,time, err] = process.argv
-let time = parseInt(time)
-time = time || 7
-let err = parseInt(err)
-err = err || 1
+let [ _N, _P ,time, err ] = process.argv
+time = parseInt(time)
+time = isNaN(time) ? 7 : time
+err = parseInt(err)
+err = isNaN(err) ? 1 : time
 console.log(`Exiting in ${time}s with error #${err}`)
 
 setTimeout(() => process.exit(err), time * 1000)
